d5e92c5333f33c81a81972832179fa89c04623df
chernikowaa <chernikovaaa143@gmail.com>
2023-07-05 17:37:34 +0000

    changess

--- spacebattle/spacebattle.cs
+++ spacebattle/spacebattle.cs
@@ -43,3 +43,67 @@ public class SpaceShip
         return corner;
     }
 }
+public class PoolGuard : IDisposable
+{
+    private static bool disposed;
+    private static List<SpaceShip> _available = new List<SpaceShip>();
+    private static List<SpaceShip> _inUse = new List<SpaceShip>();
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposed) return;
+
+        if (disposing)
+        {
+            this.Dispose();
+        }
+
+        disposed = true;
+    }
+
+    public static SpaceShip GetObject()
+    {
+        lock (_available)
+        {
+            if (_available.Count != 0)
+            {
+                SpaceShip po = _available[0];
+                _inUse.Add(po);
+                _available.RemoveAt(0);
+                return po;
+            }
+            else
+            {
+                SpaceShip po = new Ship();
+                _inUse.Add(po);
+                return po;
+            }
+        }
+    }
+
+    public static void ReleaseObject(SpaceShip po)
+    {
+        CleanUp(po);
+
+        lock (_available)
+        {
+            _available.Add(po);
+            _inUse.Remove(po);
+        }
+    }
+
+    protected static void CleanUp(SpaceShip po){
+        po.coord=new double[]{0,0}; 
+        po.speed=new double[]{0,0};
+        po.fuel_count=0;
+        po.consumption=0;
+        po.corner=0;
+        po.corner_speed=0;
+    }
+}
