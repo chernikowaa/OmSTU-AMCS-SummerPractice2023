3ce4f6abe84f5fea5eceaa6aa416e1e8dcd715c8
Ponomarev49 <ponomarevm143@gmail.com>
2023-07-05 10:32:44 +0000

    spacebattle

---
+++ spacebattle/spacebattle.cs
@@ -0,0 +1,23 @@
+﻿namespace spacebattle;
+
+public class SpaceShip
+{
+    public double[] coord = new double[2];
+    public double[] speed = new double[2];
+
+    public static double[] Move(double[] coord, double[] speed)
+    {
+        if
+        (
+            (double.IsNaN(coord[0]))||(double.IsNaN(coord[1]))||
+            (double.IsNaN(speed[0]))||(double.IsNaN(speed[1]))
+        )
+        throw new System.ArgumentException();
+        else
+        {
+            coord[0]+=speed[0];
+            coord[1]+=speed[1];
+        }
+        return coord;
+    }
+}
